2db0c51653e624e58e78865eb87a0f0b06aa519b
Daniel <danieltjandra@gmail.com>
2016-07-01 00:28:31 -0400

    Don't draw all the logs in the ui table. Takes too long if there are a lot. Instead just add a more button to load more.

--- aclient/src/main/java/dt/call/aclient/screens/LogViewer.java
+++ aclient/src/main/java/dt/call/aclient/screens/LogViewer.java
@@ -21,10 +21,15 @@ import dt.call.aclient.sqlite.DBLog;
 
 public class LogViewer extends AppCompatActivity implements View.OnClickListener
 {
-	private Button clear;
+	private static int MOREAMOUNT = 20;
+
+	private Button clear, more;
 	private CheckBox enable;
 	private LinearLayout logTable;
 	private SQLiteDb sqliteDb;
+
+	private ArrayList<DBLog> logs;
+	private int i = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -38,6 +43,8 @@ public class LogViewer extends AppCompatActivity implements View.OnClickListener
 		clear.setTag(null);
 		enable = (CheckBox)findViewById(R.id.log_viewer_enable);
 		enable.setOnClickListener(this);
+		more = (Button)findViewById(R.id.log_viewer_more);
+		more.setOnClickListener(this);
 		logTable = (LinearLayout)findViewById(R.id.log_viewer_scroller_table);
 		sqliteDb = sqliteDb.getInstance(this);
 
@@ -46,23 +53,10 @@ public class LogViewer extends AppCompatActivity implements View.OnClickListener
 			enable.setChecked(true);
 		}
 
-		ArrayList<DBLog> logs;
 		logs = sqliteDb.getLogs();
 
-		//put all logs into the table
-		for(DBLog log : logs)
-		{
-			//create a new table row for each log
-			LinearLayout logRow = (LinearLayout) View.inflate(this, R.layout.row_log_viewer, null);
-			TextView ts = (TextView)logRow.findViewById(R.id.row_log_viewer_timestamp);
-			ts.setText(log.getHumanReadableTimestampShort());
-			Button tag = (Button)logRow.findViewById(R.id.row_log_viewer_tag);
-			tag.setText(log.getTag());
-			tag.setOnClickListener(this);
-			tag.setTag(log);
-
-			logTable.addView(logRow);
-		}
+		//put the initial 20 logs in the table
+		addLogs(MOREAMOUNT);
 	}
 
 	@Override
@@ -90,12 +84,37 @@ public class LogViewer extends AppCompatActivity implements View.OnClickListener
 			logTable.removeAllViews();
 		}
 
+		if(v == more)
+		{
+			addLogs(MOREAMOUNT);
+		}
+
 		//each log entry's message is clickable
 		if(v.getTag() != null)
 		{
 			Intent seeDetails = new Intent(LogViewer.this, LogDetails.class);
 			seeDetails.putExtra(Const.EXTRA_LOG, (DBLog)v.getTag());
 			startActivity(seeDetails);
+		}
+	}
+
+	private void addLogs(int howMany)
+	{
+		int max = Math.min(i + howMany, logs.size());
+		while(i < max)
+		{
+			//create a new table row for each log
+			DBLog log = logs.get(i);
+			LinearLayout logRow = (LinearLayout) View.inflate(this, R.layout.row_log_viewer, null);
+			TextView ts = (TextView)logRow.findViewById(R.id.row_log_viewer_timestamp);
+			ts.setText(log.getHumanReadableTimestampShort());
+			Button tag = (Button)logRow.findViewById(R.id.row_log_viewer_tag);
+			tag.setText(log.getTag());
+			tag.setOnClickListener(this);
+			tag.setTag(log);
+
+			logTable.addView(logRow);
+			i++;
 		}
 	}
 }
