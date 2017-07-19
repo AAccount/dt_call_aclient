1825edd8b69dcbe732c7b87751ee763748098c22
Daniel <danieltjandra@gmail.com>
2017-07-19 00:23:47 -0400

    Document aac native functions.

--- aclient/src/main/java/dt/call/aclient/fdkaac/FdkAAC.java
+++ aclient/src/main/java/dt/call/aclient/fdkaac/FdkAAC.java
@@ -16,10 +16,22 @@ public class FdkAAC
 
 	//setup for 32kbit/s stereo @ 44100hz, CBR
 	public static native void initEncoder();
+	/**
+	 * Encode raw wav into aac
+	 * @param wav Raw wave audio. MUST be in stereo 44100Hz. In addition, the "correct" array size should be found out by getWavFrameSize()
+	 * @param aac Encoded aac audio from the wave. Should supply a cautiously large buffer.
+	 * @return Actual size of the encoded audio.
+	 */
 	public static native int encode(short[] wav, byte[] aac);
 	public static native void closeEncoder();
 
 	public static native void initDecoder();
+	/**
+	 * Decode aac audio into raw wave
+	 * @param aac Encoded aac audio. Must be the exact size. No padding at the end.
+	 * @param wav Where the raw wave will be dumped into. Should get back the size specified by getWavFrameSize()
+	 * @return Decoder errors (if there are any). Otherwise 0
+	 */
 	public static native int decode(byte[] aac, short[] wav);
 	public static native void closeDecoder();
 }
