8e02f4481bf9d671e953811150ce8244644d2bfb
Daniel <danieltjandra@gmail.com>
2018-05-20 21:38:34 -0400

    Revert "2nd try: actually get the encoding error code"
    This reverts commit dd2d261

--- aclient/src/main/jni/fdkaac-jni.c
+++ aclient/src/main/jni/fdkaac-jni.c
@@ -10,7 +10,6 @@
 
 HANDLE_AACENCODER encInternals;
 HANDLE_AACDECODER decInternals;
-AACENC_ERROR aacencError;
 
 JNIEXPORT jint JNICALL
 Java_dt_call_aclient_codec_FdkAAC_getWavFrameSize(JNIEnv *env, jclass type)
@@ -64,7 +63,6 @@ Java_dt_call_aclient_codec_FdkAAC_initAAC(JNIEnv *env, jclass type)
     {
         __android_log_print(ANDROID_LOG_ERROR, TAG, "could not retrieve encoding seed information");
     }
-    aacencError = AACENC_OK;
 
     //setup the decoder
     decInternals = aacDecoder_Open(TRANSPORT, 1);
@@ -77,7 +75,7 @@ Java_dt_call_aclient_codec_FdkAAC_initAAC(JNIEnv *env, jclass type)
 }
 
 JNIEXPORT jint JNICALL
-Java_dt_call_aclient_codec_FdkAAC_encode(JNIEnv *env, jclass type, jshortArray wav_, jbyteArray aac_)
+Java_dt_call_aclient_codec_FdkAAC_encode(JNIEnv *env, jclass type, jshortArray wav_, jbyteArray aac_, jint error_)
 {
     jshort *wav = (*env)->GetShortArrayElements(env, wav_, NULL);
 
@@ -107,7 +105,8 @@ Java_dt_call_aclient_codec_FdkAAC_encode(JNIEnv *env, jclass type, jshortArray w
     output.bufElSizes = &outputSampleSize;
     AACENC_OutArgs outArgs;
 
-    aacencError = aacEncEncode(encInternals, &input, &output, &inArgs, &outArgs);
+    int result = aacEncEncode(encInternals, &input, &output, &inArgs, &outArgs);
+    error_ = result;
     (*env)->SetByteArrayRegion(env, aac_, 0, outArgs.numOutBytes, outputBuffer);
 
     (*env)->ReleaseShortArrayElements(env, wav_, wav, 0);
@@ -143,10 +142,4 @@ JNIEXPORT void JNICALL
 Java_dt_call_aclient_codec_FdkAAC_closeDecoder(JNIEnv *env, jclass type)
 {
     aacDecoder_Close(decInternals);
-}
-
-JNIEXPORT jint JNICALL
-Java_dt_call_aclient_codec_FdkAAC_getEncodeError(JNIEnv *env, jclass type)
-{
-    return aacencError;
 }
