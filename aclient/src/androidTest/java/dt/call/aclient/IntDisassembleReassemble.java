5bac8fa8a5f12c4e96176cbb75af161a6ada3545
Daniel <danieltjandra@gmail.com>
2018-11-17 19:26:28 -0500

    Automated test for something that previously never worked properly.

---
+++ aclient/src/androidTest/java/dt/call/aclient/IntDisassembleReassemble.java
@@ -0,0 +1,27 @@
+package dt.call.aclient;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class IntDisassembleReassemble
+{
+	@Test
+	public void testDiassemblyReassembly()
+	{
+		for(int i=0; i<Integer.MAX_VALUE; i++)
+		{
+			final byte[] disaasemble = Utils.disassembleInt(i);
+			final int reassemble = Utils.reassembleInt(disaasemble);
+			Assert.assertEquals(i, reassemble);
+
+			if(i%1000000 == 0)
+			{
+				System.out.println("disassemble/reassemble int iteration: "+i);
+			}
+		}
+	}
+}
